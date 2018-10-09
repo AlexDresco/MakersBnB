02eb54773c06dfa89175a540c8c6a5d37bbff176
Ruaridhi O'Scanlain <ruaridhioscanlain@MacBook-Pro-10.local>
2018-10-09 12:35:33 +0100

    Index page tested and working

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get welcome_index_url
+    assert_response :success
+    assert_select 'a',"Log in"
+    assert_select 'a',"Sign up"
+    assert_select 'a',"View all properties"
+  end
+
+end
