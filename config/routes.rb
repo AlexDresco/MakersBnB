2a5d5013f46888b704d9e20c511ec1642edffcb8
fbl11 <freyab_london@yahoo.co.uk>
2018-10-11 10:41:13 +0100

    changed list properties to index to follow RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'welcome/login' 
   get 'listings/new', to: 'listings#new'
   post 'listings', to: 'listings#create', name: :listings_path
+  get 'listings', to: 'listings#index'
 
 
  
